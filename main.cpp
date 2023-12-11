cf6c5e9cd6d378d16995eaf1655b57be0f63e66d
alba <albanalda6@gmail.com>
2023-12-11 14:15:46 +0100

    sde

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+#include "mbed.h"
+#include "Grove_LCD_RGB_Backlight.h"
+
+
+Grove_LCD_RGB_Backlight pantalla(PB_8,PB_9);
+DigitalOut led(D13);  
+DigitalIn boton (D4);
+Timer cronometro;
+
+ int main(){
+    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+        pantalla.setRGB(0xff, 0xff, 0xff); //Ponerlo en blanco
+        while (boton == 1 ){ 
+            led = 1;
+            cronometro.start();
+        }       
+        cronometro.stop();
+
+     pantalla.print(cronometro.read());
+ }
+
